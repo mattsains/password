69cb8440c88644759a15256ce7654e060e8df755
Matthew Sainsbury <matthew@sainsbury.io>
2017-08-10 12:02:06 +0200

    Basic password details dialog

--- src/PasswordList.js
+++ src/PasswordList.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
+import FlatButton from 'material-ui/FlatButton';
 import Add from 'material-ui/svg-icons/content/add';
 import {List, ListItem} from 'material-ui/List';
 import {Card, CardTitle, CardText} from 'material-ui/Card';
 import Divider from 'material-ui/Divider';
+import Dialog from 'material-ui/Dialog';
+import RefreshIndicator from './hacks/BetterRefreshIndicator.js';
 
 import PasswordClient from './services/password-client.js';
-
-import RefreshIndicator from './hacks/BetterRefreshIndicator.js';
 
 import style from './PasswordList.css';
 
@@ -16,6 +17,7 @@ export default class PasswordList extends Component {
         super(props);
         this.state = {
             passwords: [],
+            currentPassword: undefined,
             loading: true
         };
     }
@@ -33,6 +35,8 @@ export default class PasswordList extends Component {
         if (this.state.loading) inside = <RefreshIndicator size={40} className='refresh' status='loading' />;
         else inside = <List>{this.renderListItems()}</List>;
 
+        let passwordDetailDialog = this.renderDetailsDialog();
+
         return (
             <div>
                 <FloatingActionButton className='addButton'>
@@ -41,9 +45,18 @@ export default class PasswordList extends Component {
                 <Card className='passwordList'>
                     <CardTitle title='Passwords' />
                     {inside}
+                    {passwordDetailDialog}
                 </Card>
             </div>
         );
+    }
+
+    handleDialogOpen = passwordName => {
+        this.setState({currentPassword: passwordName});
+    }
+
+    handleDialogClose = () => {
+        this.setState({currentPassword: undefined});
     }
 
     renderListItems() {
@@ -51,9 +64,30 @@ export default class PasswordList extends Component {
             <span key={item}>
             <ListItem
                 primaryText={item}
+                onTouchTap={() => this.handleDialogOpen(item)}
             />
             <Divider inset={false}/>
             </span>
         ));
+    }
+
+    renderDetailsDialog() {
+         const actions = [
+            <FlatButton
+                label="Cancel"
+                primary={true}
+                onTouchTap={this.handleDialogClose}
+            />
+        ];
+
+        return (
+            <Dialog
+                title={this.state.currentPassword}
+                modal={false}
+                actions={actions}
+                open={this.state.currentPassword}
+                onRequestClose={this.handleDialogClose}
+            />
+        );
     }
 }
